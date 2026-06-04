2f1520c3771eef378cc424d479ac06518ac2bbef
devserao <seraotourjo@gmail.com>
2026-06-04 19:30:07 +0600

    Add masonury style photo galley conataining photos, videos and document from 1971

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,25 @@ timelineButtons.forEach((button) => {
     button.classList.add("active");
   });
 });
+
+// gallery sections
+const filterButtons = document.querySelectorAll(".filter-btn");
+const galleryItems = document.querySelectorAll(".gallery-item");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    document.querySelector(".filter-btn.active").classList.remove("active");
+
+    button.classList.add("active");
+
+    const filter = button.dataset.filter;
+
+    galleryItems.forEach((item) => {
+      if (filter === "all" || item.classList.contains(filter)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
+});
